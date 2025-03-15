d43d38771012dcd19e3f73dc9d3e33ae400d5b80
aavash paudel <muproject990@gmail.com>
2025-03-15 08:30:37 +0545

    colors implemented

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -9,7 +9,14 @@ class AppColors {
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
-  // TEXt colors
+  // GradientColors
+  static const Gradient linearGradient = LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [Color(0xffff9a9e), Color(0xfffad0c4), Color(0xfffad0c4)],
+  );
+
+  // Text colors
   static const Color textPri = Color(0xFF333333);
   static const Color textSec = Color(0xFF6c757D);
   static const Color textWhite = Colors.white;
@@ -45,7 +52,4 @@ class AppColors {
   static const Color darkerGrey = Color(0xFF4f4f4f);
   static const Color darkGrey = Color(0xFF939393);
   static const Color grey = Color(0xFFe0e0e0);
-  
-
-
 }
