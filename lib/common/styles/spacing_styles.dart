0dcd98bfe028a1ea7044bb4929c787547c172fd2
aavash paudel <muproject990@gmail.com>
2025-03-16 23:21:39 +0545

    custom TSpacingStyle  for giving padding so that we dont have to write it everytime

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+
+import '../../utils/constants/sizes.dart';
+
+class TSpacingStyle{
+  static const EdgeInsetsGeometry paddingWithAppBarHeight=EdgeInsets.only(
+    top: TSize.appBarHeight,
+    left: TSize.defaultSpace,
+    bottom: TSize.defaultSpace,
+    right: TSize.defaultSpace
+  );
+
+}
